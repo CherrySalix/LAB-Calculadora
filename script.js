c79141ec57c9c45c817c112640568e7072750263
Enrique Berman <ebermansantos@gmail.com>
2025-11-19 13:44:21 -0600

    feat: finished templates for both files, pending style for html

--- script.js
+++ script.js
@@ -1,19 +1,43 @@
 function add(a, b) {
-    return a + b;
+  return a + b;
 }
 
-function subtract(a, b) {
-    return a - b;
+function substract(a, b) {
+  return a - b;
 }
+
 function multiply(a, b) {
-    return a * b;
+  return a * b;
 }
 
 function divide(a, b) {
-    if (b === 0) {
-        alert("Error: Division by zero!");
-        return null;
-    }
-    return a / b;
+  if (b === 0) {
+    return "Error: división entre 0";
+  }
+  return a / b;
 }
 
+function calcular() {
+    let num1 = parseFloat(document.getElementById("num1").value);
+    let num2 = parseFloat(document.getElementById("num2").value);
+    let operacion = document.getElementById("operacion").value;
+    let resultado;
+
+    if (isNaN(num1) || isNaN(num2)) {
+        resultado = "Ingresaste valores invalidos!";
+    }
+
+    if (operacion === "sumar") {
+        resultado = add(num1, num2);
+    }
+    else if (operacion === "restar") {
+        resultado = substract(num1, num2);
+    }
+    else if (operacion === "multiplicar") {
+        resultado = multiply(num1, num2);
+    }
+    else if (operacion === "dividir") {
+        resultado = divide(num1, num2);
+    }
+    document.getElementById("resultado").innerText = "Resultado: " + resultado;
+}
